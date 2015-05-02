a91c7cd638a702106be6bba748beb5bac5beabb0
pchev <pchev@9ea211a9-cc92-498c-ad5f-4218bad9713e>
2015-05-02 20:01:07 +0000

    Set a separate project for eqmodgui

---
+++ trunk/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+destdir=$1
+
+cpu_target=$2
+
+if [ -z "$destdir" ]; then
+   export destdir=/tmp/eqmodgui
+fi
+
+echo Install eqmodgui to $destdir
+
+install -m 755 -d $destdir
+install -m 755 -d $destdir/bin
+install -m 755 -d $destdir/share
+install -m 755 -d $destdir/share/appdata
+install -m 755 -d $destdir/share/applications
+install -m 755 -d $destdir/share/doc
+install -m 755 -d $destdir/share/doc/eqmodgui
+install -m 755 -d $destdir/share/pixmaps
+install -m 755 -d $destdir/share/icons
+install -m 755 -d $destdir/share/icons/hicolor
+install -m 755 -d $destdir/share/icons/hicolor/48x48
+install -m 755 -d $destdir/share/icons/hicolor/48x48/apps
+install -m 755 -d $destdir/share/icons/hicolor/scalable
+install -m 755 -d $destdir/share/icons/hicolor/scalable/apps
+
+install -v -m 755 -s src/eqmodgui  $destdir/bin/eqmodgui
+install -v -m 644 system_integration/Linux/share/applications/eqmodgui.desktop $destdir/share/applications/eqmodgui.desktop
+install -v -m 644 system_integration/Linux/share/pixmaps/eqmodgui.png $destdir/share/pixmaps/eqmodgui.png
+install -v -m 644 system_integration/Linux/share/icons/hicolor/48x48/apps/eqmodgui.png $destdir/share/icons/hicolor/48x48/apps/eqmodgui.png
