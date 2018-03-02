aa0ded5d058d135d3550de7416280ecb23ebf5cf
Patrick Chevalley <pch@ap-i.net>
2018-03-02 12:02:26 +0100

    Fix revision.inc path

--- git-version.sh
+++ git-version.sh
@@ -3,6 +3,6 @@
 vcount=$(git rev-list --count --first-parent HEAD)
 vnum=$(git describe --always HEAD)
 
-echo "// git revision" > src/revision.inc
-echo "const RevisionStr = '$vcount-$vnum';" >> src/revision.inc
+echo "// git revision" > revision.inc
+echo "const RevisionStr = '$vcount-$vnum';" >> revision.inc
 
